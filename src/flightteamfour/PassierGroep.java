6dfcc02847f14336e4c1db024f880a974f499532
Ian den Hartog <ian@crosspring.com>
2015-10-26 11:33:58 +0100

    begin van passagiers

--- src/flightteamfour/PassierGroep.java
+++ src/flightteamfour/PassierGroep.java
@@ -6,6 +6,15 @@ public class PassierGroep {
     
     public int passagiers;
     public Vliegveld bestemming;
+
+    public PassierGroep(int passagiers, Vliegveld bestemming) {
+        this.passagiers = passagiers;
+        this.bestemming = bestemming;
+    }
+
+    public Vliegveld getBestemming() {
+        return bestemming;
+    }
     
     
 }
--- src/flightteamfour/Vliegveld.java
+++ src/flightteamfour/Vliegveld.java
@@ -1,12 +1,16 @@
 
 package flightteamfour;
 
+import static flightteamfour.FlightTeamFour.vliegvelden;
+import java.util.ArrayList;
+
 
 public class Vliegveld {
     
     private double aantalPassagiers;
     private double aantalVracht;
     private String name;
+    private ArrayList<PassierGroep> passiersGroep = new ArrayList<PassierGroep>();
 
     public double getAantalPassagiers() {
         return aantalPassagiers;
@@ -27,7 +31,16 @@ public class Vliegveld {
         this.name = name;
         this.aantalPassagiers = aantalPassagiers;
         this.aantalVracht = aantalVracht;
+        random();
     }  
     
+    public void random(){
+        for(Vliegveld vliegveld: vliegvelden){
+            //if(vliegveld != this){
+                passiersGroep.add(new PassierGroep(100, vliegveld));
+                System.out.println("Van " + this.getName() +" Bestemming: " + passiersGroep.get(passiersGroep.size()-1).getBestemming().getName());
+            //}
+        }
+    }
 }
 
